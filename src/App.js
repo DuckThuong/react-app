cc97cb978aa499ecb121a54d1f915b70f51dd9c1
thuongtrinhduc <thuongtrinhduc@tsoft.vn>
2024-12-03 16:37:27 +0700

    adadad

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      })
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+  console.log(users);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Users List</h1>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>
+              {user.name} - {user.email}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
